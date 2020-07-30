4101709fee9049444f24813dd008a122a8320316
Ruan de Necker <ruandn@gmail.com>
2020-07-30 12:57:52 +0200

    basic registration implemented

---
+++ app/src/main/java/com/example/bloghoginclass/HomeFragment.kt
@@ -0,0 +1,38 @@
+package com.example.bloghoginclass
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+
+class HomeFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        auth = Firebase.auth
+
+        auth.addAuthStateListener { firebaseAuth ->
+            val firebaseUser = firebaseAuth.currentUser
+            if (firebaseUser == null) {
+                Log.d("Authentication", "This user is not logged in")
+                findNavController().navigate(R.id.action_homeFragment_to_registerFragment2)
+            }
+        }
+
+        return inflater.inflate(R.layout.fragment_home, container, false)
+
+    }
+
+}
--- app/src/main/java/com/example/bloghoginclass/MainActivity.kt
+++ app/src/main/java/com/example/bloghoginclass/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.bloghoginclass
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = Firebase.auth
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.logout_btn -> {
+                auth.signOut()
+                Toast.makeText(this, "Logged out", Toast.LENGTH_SHORT)
+                Log.d("Authentication", "The user was logged out")
+                return true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+        }
+    }
+
 }
